a13931b49162c462dad03f71b385a3d42db01f80
ZaitonZz <philipcandelario1@gmail.com>
2026-04-30 16:23:08 +0800

    feat: add route for upload TOR page

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::get('/', WelcomePageController::class)->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
+    Route::inertia('upload-tor', 'upload-tor')->name('uploadTor');
 });
 
 require __DIR__.'/settings.php';
